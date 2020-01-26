2a050391869a99a1cac9edf071e514f9467005b9
Emmy Cao <emmycao11@gmail.com>
2020-01-26 12:12:29 -0800

    beep boop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./scss/defaults.scss";
 import SelectionContainer from "./components/SelectionContainer.js";
 import Main from "./Main";
 
 function App() {
   const [stepState, setStepState] = React.useState(1);
-
+  useEffect(() => {
+    let reqbody = {
+      document: {
+        content: "this is a happy sentene"
+      }
+    };
+    let url = `https://language.googleapis.com/v1beta2/documents:analyzeSentiment?key=deaaacb0f4dded33393059f40b0cf3fe0ebdbfb4`;
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(reqbody)
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        console.log(response);
+      });
+  }, []);
   // const renderStart = () => (
   //   <div className="centered wrapper">
   //     <SelectionContainer type="welcome" />
--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -4,34 +4,51 @@ import ChoiceComponent from "./ChoiceComponent.js";
 
 function RenderHealth({ inputValue, setInputValue }) {
   return (
-    <div className="centered flex-col">
+    <div className="centered flex-col modal-content">
       <h1>Health question</h1>
+      <h4>
+        Your child recently transitioned and your father keeps calling him by
+        the wrong pronouns. Further, your father keeps referring to him by his
+        birth name. How do you respond?
+      </h4>
       <TextField value={inputValue} setValue={setInputValue} />
     </div>
   );
 }
 function RenderSchool({ inputValue, setInputValue }) {
   return (
-    <div className="centered flex-col">
+    <div className="centered flex-col modal-content">
       <h1>School question</h1>
+      <h4>
+        A classmate in your child’s school recently came out as gay. Your child
+        and their friends have been using the words “sissy” and “girly” to
+        describe him and constantly use the phrases “no homo” amongst
+        themselves. How do you respond to them?
+      </h4>
       <TextField value={inputValue} setValue={setInputValue} />
+      {console.log(inputValue)}
     </div>
   );
 }
 function RenderFriendship({ inputValue, setInputValue }) {
-  const choices = ["A. Choice a", "B. Choice b", "C. Choice c", "D. choice d"];
+  const choices = [
+    "A) Your fears are valid. However, if they don’t accept you, you are better off without them. True friends will cherish you for who you are.",
+    "B) You can do it! If I am being honest with you, they are not my favorite friend of yours. I do not like how they treat you. You deserve better.",
+    "C) Do not overthink it! You will be fine. You are such good friends. There is no way that they could not love the amazing person that you are.",
+    "D) You may not want to do that. You know they are religious and that will cause them to think of you differently. "
+  ];
   return (
-    <div className="centered flex-col">
+    <div className="centered flex-col modal-content">
       <h1>Friendship question</h1>
-      <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum."
-      </p>
+      <h4>
+        Your child recently came out as queer to you. They want to come out to
+        their friends of the same gender, but they are scared that their friend
+        will accuse them of having a crush on them and will not want to be
+        friends anymore. What do you tell them?
+      </h4>
+      {choices.map((value, index) => {
+        return <p className="choice">{value}</p>;
+      })}
     </div>
   );
 }
